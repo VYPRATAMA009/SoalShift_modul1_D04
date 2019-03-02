b59dc85f8299b91e20d2c2c5c63ccd0044b4140f
VYPRATAMA009 <42761818+VYPRATAMA009@users.noreply.github.com>
2019-03-02 18:38:05 +0700

    Edisi_revisi
    revisi

--- soal3.sh
+++ soal3.sh
@@ -4,12 +4,13 @@ looping=1
 
 while [ $looping -ne 0 ]
 do
-if [[ -f /home/vinsensius009/prak1/password$num.txt ]] ;
+if [[ -f /home/vinsensius009/modul1/SoalShift_modul1_D04-master/password$num.txt ]] ;
 then
 	num=$((num + 1))
 else
 
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold  -w 12 | head -1> /home/vinsensius009/prak1/password$num.txt
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold  -w 12 | head -1 |  awk '$0 ~ /[a-z]/ && $0 ~ /[0-9]/ && $0 ~ /[A-Z]/ {print $0;}' > /home/vinsensius009/modul1/SoalShift_modul1_D04-master/password$num.txt
+
 looping=0
 
 fi
